654930cb061c3047ac17883c132002c21eb8dd65
soniadiwedi <112754761+soniadiwedi@users.noreply.github.com>
2023-06-10 22:09:54 +0530

    deployment

---
+++ index.js
@@ -0,0 +1,32 @@
+const express = require("express")
+
+const cors = require("cors")
+
+
+const connection = require("./db")
+const userRouter = require("./routes/user.routes")
+const profileRoutes = require("./routes/profile")
+const emiRoutes = require("./routes/emi.routes")
+
+
+
+const app = express()
+app.use(express.json())
+app.use(cors())
+
+app.use("/auth",userRouter)
+app.use("/profile", profileRoutes);
+app.use('/emi', emiRoutes)
+
+
+
+
+app.listen(9009,async()=>{
+    try{
+        await connection
+        console.log("Server mongoose started");
+    }catch(err){
+        console.log(err);
+    }
+    console.log("Server is Running at 9009");
+})
